b7de418e1a8e5d4541acad2b33bcd312a4549b8d
Ankit Ahirwar <ahirwarankit2345@gmail.com>
2026-02-04 23:43:06 +0530

    Building & Testing Server on Same Repository

---
+++ python-server/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template_string("""
+        <h1>Hello!</h1>
+        <p>Building & Testing app on same repository.</p>
+    """)
+
+if __name__ == "__main__":
+    app.run(debug=True)
