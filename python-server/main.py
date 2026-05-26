feb514ac3cddaf89266272d39a28f6d2d1011a19
Ankit Ahirwar <ahirwarankit2345@gmail.com>
2026-05-26 12:35:35 +0530

    Post request test

--- python-server/main.py
+++ python-server/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Something(BaseModel):
+    name: str
+    price: float
 
 @app.get("/")
 async def read_root():
     return {"message": "Hello World"}
+
+@app.post("/post")
+async def post(items: Something):
+    data = {
+        "Name": items.name, 
+        "Price": items.price
+    }
+    
+    print(data)
+
+    return data
